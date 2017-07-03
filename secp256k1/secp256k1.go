6451fd888c7b6d4c285510452f045680aec199d1
jjz <woaf1003@gmail.com>
2017-07-03 18:14:43 +0800

    remove unuse ldflags

--- secp256k1/secp256k1.go
+++ secp256k1/secp256k1.go
@@ -17,7 +17,7 @@ static void freePubkeyArray(secp256k1_pubkey **a) {
         free(a);
 }
 */
-// #cgo LDFLAGS: -lsecp256k1 -lstdc++ -lm -lz -lbz2
+// #cgo LDFLAGS: -lsecp256k1
 import "C"
 
 import (
